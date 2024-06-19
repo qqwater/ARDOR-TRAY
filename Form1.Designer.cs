b726177370f9949d62747054288f252353448a98
qqwater <fastter211205@gmail.com>
2024-06-19 12:51:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ARDOR_TRAY
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            закрытьToolStripMenuItem = new ToolStripMenuItem();
+            включитьУведомленияToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { закрытьToolStripMenuItem, включитьУведомленияToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(206, 48);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            закрытьToolStripMenuItem.Size = new Size(205, 22);
+            закрытьToolStripMenuItem.Text = "Закрыть";
+            закрытьToolStripMenuItem.Click += закрытьToolStripMenuItem_Click;
+            // 
+            // включитьУведомленияToolStripMenuItem
+            // 
+            включитьУведомленияToolStripMenuItem.Name = "включитьУведомленияToolStripMenuItem";
+            включитьУведомленияToolStripMenuItem.Size = new Size(205, 22);
+            включитьУведомленияToolStripMenuItem.Text = "Включить уведомления";
+            включитьУведомленияToolStripMenuItem.Click += включитьУведомленияToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(120, 0);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem закрытьToolStripMenuItem;
+        private ToolStripMenuItem включитьУведомленияToolStripMenuItem;
+    }
+}
